3b1aa40732fe8d5f650fcebf8c78e0fac1437af7
reiniaust <55030087+reiniaust@users.noreply.github.com>
2020-07-16 16:41:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// 13.07.20 ra
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 
